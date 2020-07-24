02aa26646b189ee9499c4a6bb0150b2f73b275f8
sh4r10ck <shamsutukor@gmail.com>
2020-07-24 03:11:35 +0100

    nav var edit for department

--- adminpanel.sql
+++ adminpanel.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 19, 2020 at 01:25 PM
--- Server version: 5.7.14
--- PHP Version: 7.0.10
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jul 23, 2020 at 07:58 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -26,13 +28,15 @@ SET time_zone = "+00:00";
 -- Table structure for table `aboutus`
 --
 
-CREATE TABLE `aboutus` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `aboutus`;
+CREATE TABLE IF NOT EXISTS `aboutus` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(73) NOT NULL,
   `sub_title` varchar(73) NOT NULL,
   `description` text NOT NULL,
-  `links` varchar(73) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `links` varchar(73) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `aboutus`
@@ -45,24 +49,49 @@ INSERT INTO `aboutus` (`id`, `title`, `sub_title`, `description`, `links`) VALUE
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `dept_category`
+--
+
+DROP TABLE IF EXISTS `dept_category`;
+CREATE TABLE IF NOT EXISTS `dept_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(73) NOT NULL,
+  `description` varchar(73) NOT NULL,
+  `image` varchar(73) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `dept_category`
+--
+
+INSERT INTO `dept_category` (`id`, `name`, `description`, `image`) VALUES
+(5, 'Computer Science ', 'Department of Computer Scince', 'b571f02d5d70d97ef45036635d192c79.jpg'),
+(4, 'Computer Science ', 'Description', 'appleii-system.jpg');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `faculty`
 --
 
-CREATE TABLE `faculty` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `faculty`;
+CREATE TABLE IF NOT EXISTS `faculty` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(73) NOT NULL,
   `designation` varchar(73) NOT NULL,
   `description` varchar(73) NOT NULL,
-  `image` varchar(73) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `image` varchar(73) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=72 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `faculty`
 --
 
 INSERT INTO `faculty` (`id`, `name`, `designation`, `description`, `image`) VALUES
-(1, 'Sas', 'Economics', 'Lecturer', '349621.jpg'),
-(18, 'Sas', 'Computer Science', 'Lecturer', 'capyvara-qDBtLH0vpfg-unsplash.jpg');
+(70, 'Sas', 'Economics', 'Lecturer', '376593_394126590642483_78265321_n.jpg'),
+(71, 'Djm', 'Infotech', 'Lecturer', 'b571f02d5d70d97ef45036635d192c79.jpg');
 
 -- --------------------------------------------------------
 
@@ -70,13 +99,15 @@ INSERT INTO `faculty` (`id`, `name`, `designation`, `description`, `image`) VALU
 -- Table structure for table `register`
 --
 
-CREATE TABLE `register` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `register`;
+CREATE TABLE IF NOT EXISTS `register` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(11) NOT NULL,
   `email` varchar(17) NOT NULL,
   `password` varchar(11) NOT NULL,
-  `usertype` varchar(17) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `usertype` varchar(17) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `register`
@@ -87,48 +118,8 @@ INSERT INTO `register` (`id`, `username`, `email`, `password`, `usertype`) VALUE
 (10, 'alj', 'alj@gmail.com', '123', 'admin'),
 (11, 'gmj', 'gmj@gmail.com', '123', 'user'),
 (13, '', '', '', '');
+COMMIT;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `aboutus`
---
-ALTER TABLE `aboutus`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `faculty`
---
-ALTER TABLE `faculty`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `register`
---
-ALTER TABLE `register`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `aboutus`
---
-ALTER TABLE `aboutus`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
---
--- AUTO_INCREMENT for table `faculty`
---
-ALTER TABLE `faculty`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
---
--- AUTO_INCREMENT for table `register`
---
-ALTER TABLE `register`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
